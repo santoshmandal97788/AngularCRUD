de56cd4265722390a06c17c25cb529043f9e8d27
santoshmandal <santoshmandal97788@gmail.com>
2021-03-20 18:15:23 +0545

    Data Filtering in Angular Component

--- src/app/employees/list-employees/list-employees.component.ts
+++ src/app/employees/list-employees/list-employees.component.ts
@@ -1,3 +1,4 @@
+import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Employee } from 'src/models/employee.model';
@@ -10,16 +11,26 @@ import { EmployeeService } from '../employee.service';
 })
 export class ListEmployeesComponent implements OnInit {
 
-  employees!: Employee[];
+  employees: Employee[];
+  filteredEmployees: Employee[];
   private arrayIndex = 1;
   employeeToDisplay: any;
-  dataFromChild!: Employee;
-  searchTerm: string;
+  dataFromChild: Employee;
+  private _searchTerm: string;
+  get searchTerm(){
+    return this._searchTerm;
+  }
+  set searchTerm(value: string){
+    this._searchTerm= value;
+    this.filteredEmployees= this.filterEmployees(value);
+
+  }
   constructor(private _employeeService: EmployeeService, private _router: Router) { }
 
   ngOnInit(): void {
     this.employees = this._employeeService.getEmployees();
     this.employeeToDisplay = this.employees[0];
+    this.filteredEmployees= this.employees;
   }
 
   nextEmployee(): void {
@@ -40,12 +51,18 @@ export class ListEmployeesComponent implements OnInit {
 
   changeEmployeeName() {
     this.employees[0].name='Jordan';
+    this.filteredEmployees= this.filterEmployees(this.searchTerm);
+
     // const newEmployeeArray: Employee[] = Object.assign([], this.employees);
     // newEmployeeArray[0].name = 'Jordan';
     // this.employees = newEmployeeArray;
   }
   onMouseMove(){
-    
+
+  }
+  filterEmployees(searchString: string){
+    return this.employees.filter(employees=>
+      employees.name.toLocaleLowerCase().indexOf(searchString.toLocaleLowerCase())!==-1);
   }
 
 }
